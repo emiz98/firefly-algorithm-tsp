cd9e19497a76c957efa190dc9f5fa31deed7afef
emiz98 <64089619+emiz98@users.noreply.github.com>
2022-03-30 14:22:33 +0530

    first commit

---
+++ scripts/firefly_algorithm.js
@@ -0,0 +1,329 @@
+const params = new URL(document.location).searchParams;
+const type = params.get("type");
+const total_cities = params.get("total_cities");
+const popSize = params.get("populationSize");
+const gamma = params.get("gamma");
+const beta = params.get("beta");
+const moves = params.get("totalMoves");
+document.getElementById("title").innerHTML = type;
+if (type == "ulysses16") {
+  cities1 = ulysses16;
+}
+if (type == "ulysses22") {
+  cities1 = ulysses22;
+}
+
+// -------------Brute Force--------------------
+var order1 = [];
+var totalPermutations;
+var count1 = 0;
+
+var recordDistance1;
+var bestEver1;
+
+// --------------FireFly Algorithm--------------------
+
+var iterations = 1;
+var iterationStart = 0;
+
+var order = [];
+var cities = [];
+
+var cities1;
+
+var populationSize = 0;
+var population = [];
+var nextPopulation = [];
+var nextPopulationFitness = [];
+
+var lightAbsorption = 0.03;
+var beta0;
+
+var totalCities;
+if (type == "random") {
+  totalCities = total_cities;
+} else {
+  totalCities = cities1.length;
+}
+
+var totalMoves = 0;
+var bestEver;
+var recordDistance;
+var currentCombination = [];
+
+var DATASET_MIN_X;
+var DATASET_MAX_X;
+var DATASET_MIN_Y;
+var DATASET_MAX_Y;
+if (type != "random") {
+  DATASET_MIN_X = cities1[0].x; // 267.84
+  DATASET_MAX_X = cities1[0].x; // 267.84
+  DATASET_MIN_Y = cities1[0].y; // 84.32
+  DATASET_MAX_Y = cities1[0].y; // 84.32
+}
+
+var stability = 0;
+
+populationSize = popSize;
+lightAbsorption = gamma;
+beta0 = beta;
+totalMoves = moves;
+
+function setup() {
+  createCanvas(windowWidth - 20, windowHeight - 50);
+
+  if (type != "random") {
+    for (let city = 0; city < cities1.length; city++) {
+      if (cities1[city].x < DATASET_MIN_X) {
+        DATASET_MIN_X = cities1[city].x;
+      }
+      if (cities1[city].x > DATASET_MAX_X) {
+        DATASET_MAX_X = cities1[city].x;
+      }
+      if (cities1[city].y < DATASET_MIN_Y) {
+        DATASET_MIN_Y = cities1[city].y;
+      }
+      if (cities1[city].y > DATASET_MAX_Y) {
+        DATASET_MAX_Y = cities1[city].y;
+      }
+    }
+  }
+
+  //Original Cities
+  // for (let j = 0; j < cities1.length; j++) {
+  //   // https://stackoverflow.com/questions/69294016/how-can-i-scale-down-my-coordinates-using-map-to-fit-on-the-canvas
+  //   cities1[j].x = map(
+  //     cities1[j].x,
+  //     DATASET_MIN_X,
+  //     DATASET_MAX_X,
+  //     0 + 50,
+  //     width - 50
+  //   );
+  //   cities1[j].y = map(
+  //     cities1[j].y,
+  //     DATASET_MIN_Y,
+  //     DATASET_MAX_Y,
+  //     50,
+  //     height - 50
+  //   );
+
+  //   cities[j] = cities1[j];
+  //   order[j] = j;
+  // }
+
+  if (type != "random") {
+    for (let j = 0; j < cities1.length; j++) {
+      // https://stackoverflow.com/questions/69294016/how-can-i-scale-down-my-coordinates-using-map-to-fit-on-the-canvas
+      cities1[j].x = map(
+        cities1[j].x,
+        DATASET_MIN_X,
+        DATASET_MAX_X,
+        0 + 50,
+        width - 50
+      );
+      cities1[j].y = map(
+        cities1[j].y,
+        DATASET_MIN_Y,
+        DATASET_MAX_Y,
+        100,
+        height - 50
+      );
+      var v = createVector(
+        (cities1[j].x / 5) * 2 + 20,
+        (cities1[j].y / 5) * 3 + 50
+      );
+      cities[j] = v;
+      order[j] = j;
+      order1[j] = j;
+    }
+  }
+
+  // Random Cities
+  if (type == "random") {
+    for (var i = 0; i < totalCities; i++) {
+      var v = createVector(
+        random((width / 5) * 2 + 20),
+        random((height / 5) * 3) + 100
+      );
+      cities[i] = v;
+      order1[i] = i;
+      order[i] = i;
+    }
+  }
+
+  for (var i = 0; i < populationSize; i++) {
+    population[i] = shuffle(order);
+  }
+  bestEver = order.slice();
+  recordDistance = calcDistance(cities, order);
+
+  var d = calcDistance(cities, order1);
+  recordDistance1 = d;
+  bestEver1 = order1.slice();
+
+  totalPermutations = factorial(totalCities);
+
+  for (var i = 0; i < populationSize; i++) {
+    population[i] = shuffle(order);
+  }
+  bestEver = order.slice();
+  recordDistance = calcDistance(cities, population[0]);
+}
+
+function draw() {
+  background(0);
+
+  //---------------------Every Combination--------------------------
+  fill(255, 255, 255);
+  for (var i = 0; i < cities.length; i++) {
+    ellipse(cities[i].x, cities[i].y, 10, 10);
+    text(i, cities[i].x, cities[i].y, 70, 80);
+  }
+
+  stroke(255, 0, 0);
+  strokeWeight(2);
+  noFill();
+  beginShape();
+  for (var i = 0; i < order1.length; i++) {
+    var n = bestEver1[i];
+    vertex(cities[n].x, cities[n].y);
+  }
+  endShape();
+
+  stroke("#fff6");
+  strokeWeight(1);
+  noFill();
+  beginShape();
+  for (var i = 0; i < order1.length; i++) {
+    var n = order1[i];
+    vertex(cities[n].x, cities[n].y);
+  }
+  endShape();
+
+  var d = calcDistance(cities, order1);
+
+  if (d < recordDistance1) {
+    recordDistance1 = d;
+    bestEver1 = order1.slice();
+  }
+
+  BruteForce();
+
+  stroke(0);
+  var s1 = "";
+  var s2 = "";
+  for (let i = 0; i < order1.length; i++) {
+    s1 += order1[i];
+    s2 += bestEver1[i] + ",";
+  }
+  fill(255);
+  var percent = (count1 / totalPermutations) * 100;
+  textSize(30);
+  text(nf(percent, 0, 2) + " % Completed", 20, height - 90);
+  textSize(18);
+  text("Global Best: " + s2, 20, height - 60);
+  textSize(18);
+  text("Current Combination: " + s1, 20, height - 30);
+  textSize(25);
+  fill(255, 0, 0);
+  text("Brute Force", 20, height);
+
+  //--------------------Firefly Algorithm---------------------------
+  fill(255, 255, 255);
+  strokeWeight(0);
+  for (var i = 0; i < totalCities; i++) {
+    ellipse(cities[i].x + 800, cities[i].y, 10, 10);
+    textSize(18);
+    text(i, cities[i].x + 800, cities[i].y, 70, 80);
+  }
+
+  stroke(0, 255, 0);
+  strokeWeight(2);
+  noFill();
+  beginShape();
+  for (var i = 0; i < totalCities; i++) {
+    var n = bestEver[i];
+    vertex(cities[n].x + 800, cities[n].y);
+  }
+  endShape();
+
+  stroke("#fff6");
+  strokeWeight(1);
+  noFill();
+  beginShape();
+  for (var i = 0; i < currentCombination.length; i++) {
+    var n = currentCombination[i];
+    vertex(cities[n].x + 800, cities[n].y);
+  }
+  endShape();
+
+  textSize(18);
+  stroke(0);
+  var s1 = "";
+  var s2 = "";
+  for (let i = 0; i < order.length; i++) {
+    s1 += currentCombination[i];
+    s2 += bestEver[i] + ",";
+  }
+  fill(255);
+  text("Global Best: " + s2, 850, height - 80);
+  textSize(18);
+  text("Current Combination: " + s1, 850, height - 50);
+  textSize(25);
+  fill(0, 255, 0);
+  text("FireFly Algorithm", 850, height - 10);
+
+  FireFly();
+
+  //   if (iterationStart == iterations) {
+  //     noLoop();
+  //   }
+}
+
+function FireFly() {
+  iterationStart++;
+  for (let i = 0; i < population.length; i++) {
+    var bestFirefly = getMostAttractiveFirefly(population[i], i);
+    if (bestFirefly != null) {
+      movement(population[i], population[bestFirefly]);
+    } else {
+      var randomIndex = int(random(0, population.length));
+      movement(population[i], population[randomIndex]);
+    }
+  }
+
+  normalizeFitness();
+  pickNewPopulation();
+  getBrightestFirefly();
+
+  nextPopulation = [];
+  nextPopulationFitness = [];
+}
+
+function BruteForce() {
+  count1++;
+  var largestI = -1;
+  for (let i = 0; i < order1.length - 1; i++) {
+    if (order1[i] < order1[i + 1]) {
+      largestI = i;
+    }
+  }
+
+  if (largestI == -1) {
+    noLoop();
+    console.log("Finished");
+  }
+
+  var largestJ = -1;
+  for (let j = 0; j < order1.length; j++) {
+    if (order1[largestI] < order1[j]) {
+      largestJ = j;
+    }
+  }
+
+  swap(order1, largestI, largestJ);
+
+  var endArray = order1.splice(largestI + 1);
+  endArray.reverse();
+  order1 = order1.concat(endArray);
+}
---
+++ scripts/helpers_firefly.js
@@ -0,0 +1,124 @@
+function getMostAttractiveFirefly(fireflyI, i) {
+  var fitnessArr = [];
+
+  for (let j = 0; j < population.length; j++) {
+    fitnessArr[i] = 0;
+    if (i != j) {
+      var distance = getDistanceFAnew(fireflyI, population[j]);
+      var fitness = getFitness(distance);
+      fitnessArr[j] = fitness;
+    }
+  }
+
+  var repeatableMax = max(fitnessArr);
+  var count = 0;
+  var bestFitnessIndex;
+  for (let k = 0; k < fitnessArr.length; k++) {
+    if (repeatableMax == fitnessArr[k]) {
+      count++;
+      bestFitnessIndex = k;
+    }
+  }
+
+  if (count == 1) {
+    return bestFitnessIndex;
+  } else {
+    return null;
+  }
+}
+
+function getDistanceFAnew(fireflyI, fireflyJ) {
+  var similarEdgeCount = 0;
+
+  for (let i = 0; i < fireflyI.length - 1; i++) {
+    for (let j = 0; j < fireflyJ.length; j++) {
+      if (fireflyI[i] == fireflyJ[j] && fireflyI[i + 1] == fireflyJ[j + 1]) {
+        similarEdgeCount++;
+        break;
+      }
+    }
+  }
+
+  var arcCount = fireflyI.length - 1 - similarEdgeCount;
+  return (arcCount / totalCities) * 10;
+}
+
+function getFitness(distance) {
+  var betaTemp = beta0 * exp(-lightAbsorption * sq(distance));
+  return betaTemp;
+}
+
+function movement(moveFirefly, bestFirefly) {
+  var tempArr = moveFirefly.slice();
+  for (let i = 0; i < totalMoves; i++) {
+    var length = int(random(2, getDistanceFAnew(moveFirefly, bestFirefly)));
+    var chromosome = int(random(0, moveFirefly.length));
+    var arr_first = tempArr.slice(0, chromosome);
+    var arr_second = tempArr.slice(length + chromosome + 1);
+    var arr_middle = tempArr.slice(chromosome, chromosome + length + 1);
+    var inverseMutation = reverse(arr_middle);
+    var concat_arr = arr_first.concat(inverseMutation).concat(arr_second);
+    nextPopulation.push(concat_arr);
+
+    var fit = calculateFitness(concat_arr);
+    nextPopulationFitness.push(fit);
+  }
+}
+
+function pickNewPopulation() {
+  var tempArr = [];
+  for (let index = 0; index < populationSize; index++) {
+    var bestIndex = 0;
+    for (let i = 0; i < nextPopulationFitness.length; i++) {
+      if (nextPopulationFitness[i] > nextPopulationFitness[bestIndex]) {
+        bestIndex = i;
+      }
+    }
+    tempArr.push(nextPopulation[bestIndex]);
+    nextPopulationFitness[bestIndex] = 0;
+  }
+  population = [];
+  population = tempArr;
+}
+
+function getBrightestFirefly() {
+  for (let i = 0; i < population.length; i++) {
+    var d = calcDistance(cities, population[i]);
+    currentCombination = population[i];
+    if (d < recordDistance) {
+      stability = 0;
+      recordDistance = d;
+      bestEver = population[i].slice();
+    }
+  }
+}
+
+function calcDistance(points, order) {
+  var sum = 0;
+  for (var i = 0; i < order.length - 1; i++) {
+    var cityAIndex = order[i];
+    var cityA = points[cityAIndex];
+
+    var cityBIndex = order[i + 1];
+    var cityB = points[cityBIndex];
+    var d = dist(cityA.x, cityA.y, cityB.x, cityB.y);
+    sum += d;
+  }
+  return sum;
+}
+
+function calculateFitness(order) {
+  var dist = calcDistance(cities, order);
+  var fitnessEUC = 1 / (pow(dist, 8) + 1);
+  return fitnessEUC;
+}
+
+function normalizeFitness() {
+  var sum = 0;
+  for (var i = 0; i < nextPopulationFitness.length; i++) {
+    sum += nextPopulationFitness[i];
+  }
+  for (var i = 0; i < nextPopulationFitness.length; i++) {
+    nextPopulationFitness[i] = nextPopulationFitness[i] / sum;
+  }
+}
